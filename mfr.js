0700e56ad740ce49679df0a04a159e0e92441963
Thummala Hem Pradeep Royal <pradeeproyalthummala@Thummalas-MacBook-Pro.local>
2025-11-08 21:05:01 +0530

    02 add standardizing log

--- mfr.js
+++ mfr.js
@@ -1,6 +1,24 @@
-// Festival Ribbon count
+// 1. Festival Ribbon count
 
 const ribbons = ["red", "blue", "red", "green", "red", "blue", "blue"];
 const incrementIfBlue = (count, string) => string === 'blue' ? count + 1 : count;
-const result = ribbons.reduce(incrementIfBlue,0);
+let result = ribbons.reduce(incrementIfBlue, 0);
 console.log(result, "Festival Ribbon Count");
+
+// 2. Stargazing Log
+
+const log = [["Orion", "Leo"],
+["Taurus"],
+["Orion", "Gemini"]];
+
+const isNotIncludes = (result, x) => {
+  if (!result.includes(x)) {
+    result.push(x);
+  }
+  return result;
+}
+
+result = log
+  .flat()
+  .reduce(isNotIncludes, []);
+console.log(result)
